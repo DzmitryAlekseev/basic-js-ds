84acf7cb6f8fd2e4eb215cc5e0564d7529b2b4e6
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2023-12-23 19:12:35 +0100

    feat: defined stack function (Sut, Dec 23, 2023 19:12:34 PM)

--- src/stack.js
+++ src/stack.js
@@ -12,22 +12,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  * stack.pop(); // undefined
  *
  */
+
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor (stack){
+    this.stack = [];
+  }
+
+  push(element) {
+    return this.stack.push(element);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.stack.pop()
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.stack[this.stack.length - 1]
   }
 }
+
+const arr = new Stack();
+console.log(arr)
 
 module.exports = {
   Stack
